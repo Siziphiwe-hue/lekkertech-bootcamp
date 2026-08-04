06b7ffc63ce5fe28624afbb485f79f0cb80e80f4
SiziphiweNkweba <SiziphiweNkweaba@sanlam.com>
2026-08-04 10:21:49 +0200

    bouncer done

---
+++ task_2_bouncer.py
@@ -0,0 +1,16 @@
+guest_age = 18
+has_ticket = True
+is_vip = False
+
+age_check = guest_age >= 18
+if age_check:
+    print("Guest is old enough to enter.")
+
+if guest_age >= 18 and ( has_ticket == True):
+    print("Welcome to the club!")
+elif guest_age >=18 and not has_ticket:
+    print("You need a ticket")
+elif guest_age < 18 and is_vip:
+    print("You are not old enough to enter, but you can enter as a VIP.")
+else:
+    print("You are not old enough to enter.")
